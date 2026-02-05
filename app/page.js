0fbec25aa5fe5ba77fa393bad67e28dad4770dcd
adedgestudio-dotcom <adedgestudio@gmail.com>
2026-02-05 18:01:37 +0530

    sec-3 complete with responsive

--- app/page.js
+++ app/page.js
@@ -45,8 +45,8 @@ export default function Home() {
   return (
     <main>
       <section className="relative bg-[#d2e823] min-h-[90vh] grid grid-cols-1 lg:grid-cols-2 overflow-hidden">
-        <div className="flex justify-center items-start flex-col max-w-[720px] mx-auto lg:ml-[10vw] z-10 px-6 py-12 lg:py-0">
-          <h1 className="text-[#254f1a] font-black text-6xl md:text-[89px] leading-[0.95] tracking-tight text-center lg:text-left max-w-[600px] mt-[94px]">
+        <div className="flex justify-center items-start flex-col max-w-[720px] mx-auto lg:ml-[10vw] z-10 px-6 py-6 lg:py-0 ">
+          <h1 className="text-[#254f1a] font-black text-6xl md:text-[89px] leading-[0.95] tracking-tight text-center lg:text-left max-w-[600px] mt-8">
             A link in bio built for you.
           </h1>
 
@@ -56,7 +56,7 @@ export default function Home() {
             Instagram, TikTok, Twitter, YouTube and other social media profiles.
           </p>
 
-          <div className="input-sec flex flex-col md:flex-row gap-4 w-full max-w-2xl">
+          <div className="input-sec flex flex-col md:flex-row gap-6 w-full max-w-2xl">
             <input
               type="text"
               placeholder="linktr.ee/yourname"
@@ -70,7 +70,7 @@ export default function Home() {
 
         {/* Mobile horizontal scroll */}
         <div className="lg:hidden flex overflow-hidden pb-18">
-          <div ref={mobileScrollRef} className="flex gap-4 items-center">
+          <div ref={mobileScrollRef} className="flex gap-6 items-center">
             {/* First set */}
             <Image
               src="/pistakio.png"
@@ -253,11 +253,11 @@ export default function Home() {
           </video>
         </div>
 
-        <div className="input-sec flex flex-col gap-4 w-full max-w-2xl justify-center items-center md:items-start px-6 py-12 order-1 md:order-2">
-          <h1 className="text-[#d2e823] font-black text-4xl md:text-[60px] leading-[0.95] tracking-tight text-center md:text-left max-w-[600px]">
+        <div className="input-sec flex flex-col gap-6 w-full max-w-2xl justify-center items-center md:items-start px-6 py-12 order-1 md:order-2">
+          <h1 className="text-[#d2e823] font-black text-[42px] md:text-[64px] leading-[0.95] tracking-tight text-center md:text-left max-w-[600px]">
             Create and customize your Linktree in minutes
           </h1>
-          <p className="text-white font-medium text-base sm:text-[22px] py-6 text-center md:text-left">
+          <p className="text-white font-medium  text-[16px] sm:text-[22px] text-center md:text-left">
             Connect all your content across social media, websites, stores and
             more in one link in bio. Customize every detail or let Linktree
             automatically enhance it to match your brand and drive more clicks.
@@ -268,26 +268,26 @@ export default function Home() {
         </div>
       </section>
       <section className="sec-3 bg-[#780016] relative min-h-[90vh] grid grid-cols-1 lg:grid-cols-2 overflow-hidden">
-        <div className="flex justify-center items-start flex-col max-w-[720px] mx-auto lg:ml-[10vw] z-10 px-6 py-12 lg:py-0">
-          <h1 className="text-[#e9c0e9] font-black text-6xl md:text-[62px] leading-[0.95] tracking-tight text-center lg:text-left max-w-[600px] mt-[94px]">
+        <div className="flex justify-center items-start flex-col gap-6 max-w-[720px] mx-auto lg:ml-[10vw] z-10 px-6 py-8 lg:py-0">
+          <h1 className="text-[#e9c0e9] font-black  text-[40px] md:text-[62px] leading-[0.95] tracking-tight text-center lg:text-left max-w-[600px] mt-8 lg:mt-[94px]">
             Share your Linktree anywhere you like!
           </h1>
 
-          <p className="text-[#e9c0e9] font-medium py-6 text-lg sm:text-base text-center lg:text-left">
+          <p className="text-white font-medium text-[18px] sm:text-base text-center lg:text-left">
             Add your unique Linktree URL to all the platforms and places you
             find your audience. Then use your QR code to drive your offline
             traffic back to your link in bio.
           </p>
 
-          <div className="input-sec flex flex-col md:flex-row gap-4 w-full max-w-2xl">
+          <div className="input-sec flex flex-col md:flex-row gap-6 w-full max-w-2xl">
             <button className="bg-[#e9c0e9] hover:bg-[#ea98ea] text-black px-8 py-4 text-lg font-bold rounded-full whitespace-nowrap transition-colors  ">
               Get started for free
             </button>
           </div>
         </div>
 
-        <div className="order-2 lg:order-1 flex justify-center">
-          <div className="w-full max-w-lg h-[600px]">
+        <div className="order-2 lg:order-1 flex justify-center items-center py-8 lg:py-0">
+          <div className="w-full max-w-lg h-[400px] lg:h-[600px]">
             <RiveAnimation />
           </div>
         </div>
