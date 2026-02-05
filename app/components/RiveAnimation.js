bf78cf4650334755037ccf9ea0836fe8f8ee955f
adedgestudio-dotcom <adedgestudio@gmail.com>
2026-02-05 17:13:23 +0530

    third section completed

--- app/components/RiveAnimation.js
+++ app/components/RiveAnimation.js
@@ -3,15 +3,9 @@ import { useRive, Layout, Fit, Alignment } from "@rive-app/react-canvas";
 
 export default function RiveAnimation() {
   const { RiveComponent } = useRive({
-    // Use Linktree's .riv file URL
     src: "https://cdn.prod.website-files.com/666255f7f2126f4e8cec6f8f/680faadc793de4a67df69d04_homepage_image_3.riv",
-
-    // Animation settings (from the data attributes you showed)
     stateMachines: "State Machine",
-    artboard: "Homepage_image 3",
     autoplay: true,
-
-    // Layout settings
     layout: new Layout({
       fit: Fit.Contain,
       alignment: Alignment.Center,
